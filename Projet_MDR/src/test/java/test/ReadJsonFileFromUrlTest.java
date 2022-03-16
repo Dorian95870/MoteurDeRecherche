fe16b5640629de31170eb3fe3244b98101e4e546
RQ2311 <steevenkang2311@gmail.com>
2022-03-16 13:14:28 +0100

    Commentaires sur la récupération des livres via JSON

--- Projet_MDR/src/test/java/test/ReadJsonFileFromUrlTest.java
+++ Projet_MDR/src/test/java/test/ReadJsonFileFromUrlTest.java
@@ -7,12 +7,20 @@ import java.io.InputStreamReader;
 import java.io.Reader;
 import java.net.URL;
 import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 public class ReadJsonFileFromUrlTest {
+	  /**
+	   * Read all the file char by char
+	   * @param rd
+	   * @return
+	   * @throws IOException
+	   */
 	  private static String readAll(Reader rd) throws IOException {
 		    StringBuilder sb = new StringBuilder();
 		    int cp;
@@ -21,8 +29,15 @@ public class ReadJsonFileFromUrlTest {
 		    }
 		    return sb.toString();
 		  }
-
-		  public static JSONObject readJsonFromUrl(String url) throws IOException, JSONException {
+	  
+	  	/**
+	  	 * Read Json File From an URL
+	  	 * @param url
+	  	 * @return
+	  	 * @throws IOException
+	  	 * @throws JSONException
+	  	 */
+		public static JSONObject readJsonFromUrl(String url) throws IOException, JSONException {
 		    InputStream is = new URL(url).openStream();
 		    try {
 		    	BufferedReader rd = new BufferedReader(new InputStreamReader(is, Charset.forName("UTF-8")));
@@ -35,15 +50,82 @@ public class ReadJsonFileFromUrlTest {
 		    }
 		  }
 		  
-		  public static JSONArray getBooks(JSONObject json) { //but dcomposer les results obtenu plus bas
+		  /**
+		   * get the books on the json file
+		   * @param json
+		   * @return JSONArray with all the books as JSONObject
+		   */
+		  public static JSONArray getBooks(JSONObject json) { //but décomposer les results obtenus plus bas
 			  JSONArray results = (JSONArray) json.get("results");
 			  return results;
 		  }
 		  
+		  /**
+		   * Get books titles on the current JSON page
+		   * @param jsonArray
+		   * @return
+		   */
+		  public static HashMap<Integer,String> getBooksTitle(JSONArray jsonArray){
+			  HashMap<Integer,String> books_IdTitleOnPage = new HashMap<Integer,String>();
+			  for (int i = 0; i<jsonArray.length(); i++) {
+				  int bookId = (int) ((JSONObject)jsonArray.get(i)).get("id");
+				  String bookTitle = (String) ((JSONObject)jsonArray.get(i)).get("title");
+				  books_IdTitleOnPage.put(bookId, bookTitle);
+			  }
+			  return books_IdTitleOnPage;
+		  }
+		  
+		  /**
+		   * Get all books Title on project gutenberg
+		   * @param json
+		   * @return HashMap(bookID,title)
+		   * @throws JSONException
+		   * @throws IOException
+		   */
+		  public static HashMap<Integer,String> getAllBooksTitle(JSONObject json) throws JSONException, IOException{
+			  HashMap<Integer,String> books_IdTitle = new HashMap<Integer,String>();
+			  JSONObject currentJsonPage = json;
+			  while(!currentJsonPage.get("next").equals("null")) {
+				  JSONArray booksOnPage = getBooks(currentJsonPage);
+				  books_IdTitle = getBooksTitle(booksOnPage);
+				  System.out.println(books_IdTitle.toString());
+				  currentJsonPage = readJsonFromUrl(currentJsonPage.get("next").toString());
+			  }
+			return books_IdTitle;
+		  }
+		  
+		  
+		  /**
+		   * Get all books Title on project gutenberg
+		   * @param json
+		   * @return ArrayList with all project gutenberg's books titles
+		   * @throws JSONException
+		   * @throws IOException
+		   */
+		  public static ArrayList<String> getAllBooksTitleInArray(JSONObject json) throws JSONException, IOException{
+			  ArrayList<String> titles = new ArrayList<String>();
+			  JSONObject currentJsonPage = json;
+			  while(!currentJsonPage.get("next").equals("null")) {
+				  JSONArray booksOnPage = getBooks(currentJsonPage);
+				  for (int i = 0; i<booksOnPage.length(); i++) {
+					  String bookTitle = (String) ((JSONObject)booksOnPage.get(i)).get("title");
+					  titles.add(bookTitle);
+				  }
+				  System.out.println(titles.toString());
+				  currentJsonPage = readJsonFromUrl(currentJsonPage.get("next").toString());
+			  }
+			return titles;
+		  }
+		  
 		  public static void main(String[] args) throws IOException, JSONException {
 			  JSONObject json = readJsonFromUrl("https://gutendex.com/books/");
-			  JSONArray results = getBooks(json);
-			  System.out.println(((JSONObject)results.get(0)).get("title"));
+			  //JSONArray booksOnPage = getBooks(json);
+			  //HashMap<Integer,String> booksTitles = getBooksTitle(booksOnPage);
+			  //HashMap<Integer,String> booksTitles = getAllBooksTitle(json);
+			  ArrayList<String> titles = getAllBooksTitleInArray(json);
+			  System.out.println(titles.toString());
+			  //System.out.println(booksTitles.toString());
+			  //System.out.println(((JSONObject)booksOnPage.get(0)).get("title"));
 			  //System.out.println(json.get("results")); //trop grand pour être affiché 
 			  //System.out.println(json.toString()); //trop grand pour être affiché
 		    
