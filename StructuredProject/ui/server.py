31aac543d56a2abce4cd41a7d11f0f50c5731df2
RQ2311 <steevenkang2311@gmail.com>
2022-04-05 12:24:37 +0200

    Affichage des id des livre après requête

--- StructuredProject/ui/server.py
+++ StructuredProject/ui/server.py
@@ -1,4 +1,7 @@
-from flask import Flask, render_template, request, session, redirect
+import imp
+from pydoc import doc
+import numpy as np
+from flask import Flask, render_template, request, session, redirect , jsonify
 from filtering import filterByRequest
 
 app = Flask(__name__)
@@ -9,12 +12,23 @@ def index():
     if type(userRequest) != type(None):
         docs, count = filterByRequest(app.config['myIndex'], userRequest)
         app.config['results'] = count
+        app.config['listOfBooks'] = docs
         return redirect("/result")
     return render_template('index.html')
 
 @app.route('/result')
 def result():
-    return str(app.config['results']) + " résultats !"
+    BooksList = ""
+    for x in range(len(app.config['listOfBooks'])):
+        #print(app.config['listOfBooks'][x])
+        val = app.config['listOfBooks'][x]
+        pyval = val.item()
+        BooksList = BooksList + str(pyval) + '\n'
+    
+    print('\n')
+    print(BooksList)
+    return str(app.config['results']) + " résultats ! " + BooksList
+    
 
 def startServer(index):
     app.config['myIndex'] = index
