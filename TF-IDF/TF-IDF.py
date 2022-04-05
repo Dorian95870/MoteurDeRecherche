35e77c9f725bcb39327399a040d2388e1a7af968
Dorian95870 <79307259+Dorian95870@users.noreply.github.com>
2022-04-05 12:20:24 +0200

    Update TF-IDF

--- TF-IDF/TF-IDF.py
+++ TF-IDF/TF-IDF.py
@@ -1,26 +1,19 @@
 import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
 
-documentA = 'this man is eating an apple'
-documentB = 'this child loves to read books'
+documentA = 'The Declaration of Independence of the United States of Americ The United States Bill of Rights The Ten Original Amendments to the Constitution of the United States John F. Kennedys Inaugural Address Lincolns Gettysburg Address Given November 19, 1863 on the battlefield near Gettysburg, Pennsylvania, USA The United States Constitution Give Me Liberty or Give Me Death The Mayflower Compact Abraham Lincolns Second Inaugural Address Abraham Lincolns First Inaugural Address The King James Version of the Bible Alices Adventures in Wonderland Through the Looking-Glass The Hunting of the Snark: An Agony in Eight Fits The 1990 CIA World Factbook Moby-Dick; or, The Whale Peter Pan The Book of Mormon An Account Written by the Hand of Mormon, Upon Plates Taken from the Plates of Nephi The Federalist Papers The Song of Hiawatha Paradise Lost'
 
 # Extraire les caractéristiques du texte
 bagOfWordsA = documentA.split(' ')
-bagOfWordsB = documentB.split(' ')
 
 # Supprime automatiquement tout mot en double.
-uniqueWords = set(bagOfWordsA).union(set(bagOfWordsB))
+uniqueWords = set(bagOfWordsA).union(set(bagOfWordsA))
 
-# Créer un sdictionnaire des mots et de leur occurrence pour chaque document du corpus
+# Créer un dictionnaire des mots et de leur occurrence pour chaque document du corpus
 numOfWordsA = dict.fromkeys(uniqueWords, 0)
 
 for word in bagOfWordsA:
     numOfWordsA[word] += 1
-
-numOfWordsB = dict.fromkeys(uniqueWords, 0)
-
-for word in bagOfWordsB:
-    numOfWordsB[word] += 1
 
 def computeTF(wordDict, bagOfWords):
     tfDict = {}
@@ -30,7 +23,6 @@ def computeTF(wordDict, bagOfWords):
     return tfDict
 
 tfA = computeTF(numOfWordsA, bagOfWordsA)
-tfB = computeTF(numOfWordsB, bagOfWordsB)
 
 def computeIDF(documents):
     import math
@@ -46,7 +38,7 @@ def computeIDF(documents):
         idfDict[word] = math.log(N / float(val))
     return idfDict
 
-idfs = computeIDF([numOfWordsA, numOfWordsB])
+idfs = computeIDF([numOfWordsA])
 
 def computeTFIDF(tfBagOfWords, idfs):
     tfidf = {}
@@ -55,12 +47,11 @@ def computeTFIDF(tfBagOfWords, idfs):
     return tfidf
 
 tfidfA = computeTFIDF(tfA, idfs)
-tfidfB = computeTFIDF(tfB, idfs)
 
-df = pd.DataFrame([tfidfA, tfidfB])
+df = pd.DataFrame([tfidfA])
 
 vectorizer = TfidfVectorizer()
-vectors = vectorizer.fit_transform([documentA, documentB])
+vectors = vectorizer.fit_transform([documentA])
 feature_names = vectorizer.get_feature_names()
 dense = vectors.todense()
 denselist = dense.tolist()
