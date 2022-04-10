5c1babd5a4d1ec3cf9522ce1932d719770791048
Ym11111 <91197078+Ym11111@users.noreply.github.com>
2022-04-10 22:58:48 +0200

    Update testIndex.py

--- StructuredProject/test/testIndex.py
+++ StructuredProject/test/testIndex.py
@@ -4,7 +4,7 @@ import unittest
 class TestIndex(unittest.TestCase):
   
   # tester si l'id du mot bookshop est 172
-  def test_position(self):
+  def test_Id(self):
     self.assertEqual('bookshop',self.data['Text#']==172)
 
 
