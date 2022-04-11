f9ee29f005c73b77f5f4ca7cbf0c4285910c9a6d
Mathieu RODRIGUES <mathieudomingues925@gmail.com>
2022-04-11 22:04:22 +0200

    Add 'AND' Filter + Some Fixes

--- StructuredProject/filtering/filter.py
+++ StructuredProject/filtering/filter.py
@@ -3,13 +3,36 @@ import re
 
 def filterByRequest(dataIndex, request :str):
   #Definir nos règles de Filtrage
-  docs = list()
   
   wordsWithoutPonctuation = re.sub(r'[^\w\s]', '', request)
   words = wordsWithoutPonctuation.lower().split(' ')
-  for i in range(len(words)):
-    try:
-      docs += dataIndex[words[i]]
-    except:
-      print('Aucun résulats pour la requete: {}'.format(words[i]))
-  return docs, len(docs)
+
+  def getResOR():
+    docsOR = list()
+    for i in range(len(words)):
+      try:
+        docsOR += dataIndex[words[i]]
+      except:
+        print('Aucun résulats pour la requete: {}'.format(words[i]))
+    return docsOR
+
+  def getResAND():
+    def intersection(lst1, lst2):
+      lst3 = [value for value in lst1 if value in lst2]
+      return lst3
+
+    docsAND = list()
+    for i in range(len(words)):
+      try:
+        docsAND.append(dataIndex[words[i]])
+      except:
+        print('Aucun résulats pour la requete: {}'.format(words[i]))
+    res = list()
+    for i in range(len(docsAND)):
+      try:
+        res = intersection(docsAND[i-1], docsAND[i])
+      except:
+        res = intersection(docsAND[i-1], docsAND[i])
+    return res
+
+  return (getResOR(), getResAND())
