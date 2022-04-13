56abd454b78fda8a1ecee86d470ca3d1ef899b8b
Dorian95870 <79307259+Dorian95870@users.noreply.github.com>
2022-04-13 23:28:10 +0200

    fix td-idf

--- StructuredProject/termFrequency/tfIdf.py
+++ StructuredProject/termFrequency/tfIdf.py
@@ -1,69 +1,63 @@
 import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
 
-documentA = 'this man is eating an apple'
-documentB = 'this child loves to read books'
+data = pd.read_csv("pg_catalog.csv")
 
-# Extraire les caractéristiques du texte
-bagOfWordsA = documentA.split(' ')
-bagOfWordsB = documentB.split(' ')
+title = 'Title'
 
-# Supprime automatiquement tout mot en double.
-uniqueWords = set(bagOfWordsA).union(set(bagOfWordsB))
+root = data[title][range(0,67583)]
 
-# Créer un sdictionnaire des mots et de leur occurrence pour chaque document du corpus
-numOfWordsA = dict.fromkeys(uniqueWords, 0)
 
-for word in bagOfWordsA:
+for i in range(len(root)):
+  documentA =  root[i]
+  bagOfWordsA = documentA.split(' ')
+  uniqueWords = set(bagOfWordsA).union(set(bagOfWordsA))
+  numOfWordsA = dict.fromkeys(uniqueWords, 0)
+
+  for word in bagOfWordsA : 
     numOfWordsA[word] += 1
-
-numOfWordsB = dict.fromkeys(uniqueWords, 0)
-
-for word in bagOfWordsB:
-    numOfWordsB[word] += 1
-
-def computeTF(wordDict, bagOfWords):
-    tfDict = {}
-    bagOfWordsCount = len(bagOfWords)
-    for word, count in wordDict.items():
+    
+  def computeTF(wordDict, bagOfWords):
+      tfDict = {}
+      bagOfWordsCount = len(bagOfWords)
+      for word, count in wordDict.items():
         tfDict[word] = count / float(bagOfWordsCount)
-    return tfDict
+      return tfDict
 
-tfA = computeTF(numOfWordsA, bagOfWordsA)
-tfB = computeTF(numOfWordsB, bagOfWordsB)
+  tfA = computeTF(numOfWordsA, bagOfWordsA)
 
-def computeIDF(documents):
-    import math
-    N = len(documents)
-    
-    idfDict = dict.fromkeys(documents[0].keys(), 0)
-    for document in documents:
-        for word, val in document.items():
-            if val > 0:
-                idfDict[word] += 1
-    
-    for word, val in idfDict.items():
-        idfDict[word] = math.log(N / float(val))
-    return idfDict
+  def computeIDF(documents):
+      import math
+      N = len(documents)
+      
+      idfDict = dict.fromkeys(documents[0].keys(), 0)
+      for document in documents:
+          for word, val in document.items():
+              if val > 0:
+                  idfDict[word] += 1
+      
+      for word, val in idfDict.items():
+          idfDict[word] = math.log(N / float(val))
+      return idfDict
 
-idfs = computeIDF([numOfWordsA, numOfWordsB])
+  idfs = computeIDF([numOfWordsA])
 
-def computeTFIDF(tfBagOfWords, idfs):
-    tfidf = {}
-    for word, val in tfBagOfWords.items():
-        tfidf[word] = val * idfs[word]
-    return tfidf
+  def computeTFIDF(tfBagOfWords, idfs):
+      tfidf = {}
+      for word, val in tfBagOfWords.items():
+          tfidf[word] = val * idfs[word]
+      return tfidf
 
-tfidfA = computeTFIDF(tfA, idfs)
-tfidfB = computeTFIDF(tfB, idfs)
+  tfidfA = computeTFIDF(tfA, idfs)
 
-df = pd.DataFrame([tfidfA, tfidfB])
-
-vectorizer = TfidfVectorizer()
-vectors = vectorizer.fit_transform([documentA, documentB])
-feature_names = vectorizer.get_feature_names()
-dense = vectors.todense()
-denselist = dense.tolist()
-df = pd.DataFrame(denselist, columns=feature_names)
-
-df.head()
+  try : 
+    df = pd.DataFrame([tfidfA])
+    vectorizer = TfidfVectorizer()
+    vectors = vectorizer.fit_transform([documentA])
+    feature_names = vectorizer.get_feature_names()
+    dense = vectors.todense()
+    denselist = dense.tolist()
+    df = pd.DataFrame(denselist, columns=feature_names)
+    print(df)
+  except :
+    print('marche pas')
