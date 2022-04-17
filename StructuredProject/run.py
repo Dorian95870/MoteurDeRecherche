8d17178a3eefb4c670db15e4617cac26358a2113
Mathieu RODRIGUES <74418584+MathieuRodri@users.noreply.github.com>
2022-04-17 23:30:22 +0200

    Cleaning

--- StructuredProject/run.py
+++ StructuredProject/run.py
@@ -6,14 +6,5 @@ from ranking import *
 
 data = getData()
 dataIndex = getIndex(data)
-#print(dataIndex)
-
-#res = getBooksByListOfIds(data, [2, 16245])
-#print(res['Authors'].values)
-
-#fOR, fAND = filterByRequest(dataIndex, "Ten Original")
-#print(fOR, fAND)
-
-#rankingByAndOr(dataIndex, "scarlet car")
 
 startServer(dataIndex, data)
